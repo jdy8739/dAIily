b9302d5f0297c9c2c8aa42b0a12bf49963c62d2c
jdy8739 <jdy8739@naver.com>
2025-09-20 21:14:01 +0900

    project init

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+
+import "./globals.css";
+
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang="en">
+      <body
+       
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
